e6cbd98c7e70543c1d215ecb2fc733245a34f6d5
toantt <toantt@vmogroup.com>
2023-07-21 14:40:00 +0700

    fix(backend): auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::API
         header = request.headers['Authorization']
         header = header.split(' ').last if header
         begin
-          @decoded = JWT.decode(header, ENV['secret_key_base'])
-          @current_user = User.where(@decoded[0][:user_id]).first()
-          jti = @decoded[0][:jti]
+          @decoded = JWT.decode(header, Rails.application.secrets.secret_key_base)
+          @current_user = User.where(id: @decoded[0]['user_id']).first()
+          jti = @decoded[0]['jti']
           if BlacklistedToken.exists?(jti: jti)
             render json: { error: 'Invalid token' }, status: :unauthorized
           else
